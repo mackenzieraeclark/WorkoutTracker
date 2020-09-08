c0a30a019e77a1e4045e8ad9f40693da9e46cfe8
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-08 02:44:27 -0400

    model for exercise schema

---
+++ models/workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  name: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+  },
+  sets: {
+    type: Number,
+  },
+  reps: {
+    type: Number,
+  },
+  duration: 
+    {
+    type: Number,
+  },
+  distance: {
+    type: Number,
+  },
+});
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date, 
+    default: Date.now,
+  },
+  exercises: [ExerciseSchema],
+}, { toJSON: {
+    virtuals: true
+} }
+);
+WorkoutSchema.virtual('totalDuration').get(function() {
+    return this.exercises.reduce((total, exerecise) => {
+        return total + exerecise.duration
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
