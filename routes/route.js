2196720bddb6674b8cb7c13e00f2f400105b0457
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-08 03:28:25 -0400

    routes

---
+++ routes/route.js
@@ -0,0 +1,18 @@
+const app = require("express").Router();
+const path = require("path");
+
+// Use GET to direct to correct paths for client output
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/exercise.html"))
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/stats.html"))
+});
+
+module.exports = app;
