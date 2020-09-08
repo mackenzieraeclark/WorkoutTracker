aa66d0192ed1a2553d08c95f6b3074b0f65506ca
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-08 13:55:47 -0400

    require routes

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3030
 const app = express();
 
-//Use Express to set app
+// Use Express to set app
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
-//Connect mongoose
+// Connect mongoose
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
 });
+
+// Require routes
+app.use(require("/routes/api-route.js"));
+app.use(require("/routes/route.js"));
