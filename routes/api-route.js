01861ffdb50b9b5f944bb8ae242fb2a43ef6246c
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-08 03:40:58 -0400

    create body

--- routes/api-route.js
+++ routes/api-route.js
@@ -2,6 +2,13 @@ const app = require("express").Router();
 const workout = require("/models/workout.js");
 
 // POST a new workout
+app.post("/api/workouts", ({body}, res) => {
+    workout.create(body).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 //GET the most recent workout
 
