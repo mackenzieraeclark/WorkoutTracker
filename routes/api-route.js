72edb3ae63ee2748bb3972505920544871959a87
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-08 03:35:28 -0400

    set up needed api routes

---
+++ routes/api-route.js
@@ -0,0 +1,10 @@
+const app = require("express").Router();
+const workout = require("/models/workout.js");
+
+// POST a new workout
+
+//GET the most recent workout
+
+//PUT in a new exercise
+
+//GET daily workouts for chart info
